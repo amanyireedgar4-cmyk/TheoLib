988ccf62ce6fba3f081330558b840f038f5c4ce6
amanyireedgar4-cmyk <amanyireedgar4@gmail.com>
2026-01-06 13:37:33 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyC7LjxapWLQCW_zLRCOZS25hWVEaOG1mFY",
-    authDomain: "theolib-426cf.firebaseapp.com",
-    projectId: "theolib-426cf",
-    storageBucket: "theolib-426cf.firebasestorage.app",
-    messagingSenderId: "879359514394",
-    appId: "1:879359514394:web:259ad5ed2e8949e2e97ea3"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyC7LjxapWLQCW_zLRCOZS25hWVEaOG1mFY",
+  authDomain: "theolib-426cf.firebaseapp.com",
+  projectId: "theolib-426cf",
+  storageBucket: "theolib-426cf.firebasestorage.app",
+  messagingSenderId: "879359514394",
+  appId: "1:879359514394:web:259ad5ed2e8949e2e97ea3"
+};
 
-  firebase.initializeApp(firebaseConfig);
-
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
