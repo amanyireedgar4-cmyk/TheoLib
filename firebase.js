4b49b40e7e1b00dc9251e96ca63886aa737a789c
amanyireedgar4-cmyk <amanyireedgar4@gmail.com>
2026-01-11 06:44:41 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,4 @@
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -10,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC7LjxapWLQCW_zLRCOZS25hWVEaOG1mFY",
   authDomain: "theolib-426cf.firebaseapp.com",
   projectId: "theolib-426cf",
-  storageBucket: "theolib-426cf.firebasestorage.app",
+  storageBucket: "theolib-426cf.appspot.com",
   messagingSenderId: "879359514394",
   appId: "1:879359514394:web:259ad5ed2e8949e2e97ea3"
 };
@@ -18,3 +15,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+</script>
